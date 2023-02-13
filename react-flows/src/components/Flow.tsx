e16dc014d101cf33d72c322b4e902d08eac38294
santosfk <santosguilhermefk@gmail.com>
2023-02-12 22:41:27 -0300

    fix node types

--- react-flows/src/components/Flow.tsx
+++ react-flows/src/components/Flow.tsx
@@ -1,7 +1,41 @@
-import ReactFlow, { Controls, Background } from "reactflow";
+import { useCallback, useState } from "react";
+import ReactFlow, {
+  Controls,
+  Background,
+  applyEdgeChanges,
+  applyNodeChanges,
+  EdgeChange,
+} from "reactflow";
 import "reactflow/dist/style.css";
+import { Edge, Node } from "reactflow";
+const initialNodes = [
+  {
+    id: "1",
+    data: { label: "Hello" },
+    position: { x: 0, y: 0 },
+    type: "input",
+  },
+  {
+    id: "2",
+    data: { label: "World" },
+    position: { x: 100, y: 100 },
+  },
+];
 
+const initialEdges = [
+  { id: "1-2", source: "1", target: "2", label: "to the", type: "step" },
+];
 function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+  const onNodesChange = useCallback(
+    (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
   return (
     <div style={{ height: "100%" }}>
       <ReactFlow>
